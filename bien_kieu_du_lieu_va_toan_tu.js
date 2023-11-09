4466d05d2be2a9171483a9774c5d73df2914909b
vandat3012 <luongvandat301220@gmail.com>
2023-11-09 17:05:35 +0700

    update bai tap

---
+++ bien_kieu_du_lieu_va_toan_tu.js
@@ -0,0 +1,22 @@
+
+let diemToan = parseInt(prompt('Enter Toan'));
+let diemHoa = parseInt(prompt('Enter Hoa'));
+let diemLy = parseInt(prompt('Enter ly'));
+let average = (diemToan+diemHoa+diemLy)/3;
+alert(average);
+let total = diemToan+diemLy+diemHoa;
+alert(total);
+//bài 2 độ C qua độ F
+let f=parseInt(prompt('Độ f'));
+let c=5*(32-f)/9;
+alert(c);
+// bài 3 chu vi hìh tròn
+let a=3.14;
+let b =parseInt(prompt("Ban kih"));
+let perimeter=2*a*b;
+alert(perimeter);
+// bài 4 tính S hìh tròn
+let x =parseInt(prompt('Độ dài'));
+let y=parseInt(prompt('Độ rộg'));
+let area=x*y;
+alert(area);
